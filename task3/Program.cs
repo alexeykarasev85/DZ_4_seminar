0f9701ae43a4f2f27913790caed88f6e37d05f23
Alexey <alexeykarasev85@gmail.com>
2023-06-25 23:44:29 +0300

    ДЗ по 4му семинару + 1я задача из предыдущего семинара, которая не открылась

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for (int i = 1; i <= number; i++)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+}
+
+Console.WriteLine(sum);
+
+
+
---
+++ task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// Первый вариант, рандомной кол-во элементов массива
+// int[] arr = new int[8];
+// Random rnd = new Random();
+// for (int i = 0; i < arr.Length; i++)
+// {
+// arr[i] = rnd.Next(0,100);
+// // System.Console.Write($"{arr[i]} ");
+// }
+// System.Console.WriteLine($"[{string.Join("; ", arr)}]");
+
+
+// Второй вариант, когда пользователь сам вводит кол-во элементов массива
+Console.Write("Введите количество элементов массива Х: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Length = number;
+int[] arr = new int[Length];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(0,100);
+// System.Console.Write($"{arr[i]} ");
+}
+System.Console.WriteLine($"[{string.Join("; ", arr)}]");
+
